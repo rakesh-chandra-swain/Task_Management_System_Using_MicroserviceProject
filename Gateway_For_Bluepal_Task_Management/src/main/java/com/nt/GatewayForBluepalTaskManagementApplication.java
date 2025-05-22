c69eb826b7990b941676eacaa277bf794d714cb0
Rakesh Chandra Swain <chandrarakesh2001@gmail.com>
2025-05-22 14:54:52 +0530

    Microservice

---
+++ Gateway_For_Bluepal_Task_Management/src/main/java/com/nt/GatewayForBluepalTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayForBluepalTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayForBluepalTaskManagementApplication.class, args);
+	}
+
+}
